4fc047a98b9832fb5b5c12e66280ad0115736904
keinokalle <keinokalle@gmail.com>
2023-04-14 22:19:18 +0300

    Pieniä lisäyksiä, ei mitään isoa

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react"
 import { Slider } from "./Slider/Slider";
+import "./styles.css"
 
 
 export default function  Home() {
 
     // Tästä alkavat slideriin ja interaktiiviseen kyselyyn liittyvät arvot ja funktiot
-    const statementList = [{statement: "Ajan autolla töihin", value: 50}, {statement: "Syön kasvisruokaa", value: 50}]
+    const statementList = [{statement: "En aja autolla töihin", value: 50}, {statement: "Syön kasvisruokaa", value: 50}, {statement: "En matkusta lentokoneella", value: 50}]
     const feedBack = [
         "Olet ilmastontuhoaja. Koita parantaa tapasi",
-        "Voisit tsempata paljon.",
         "Olet aika normaali",
-        "Teet jo jotain hyvää",
+        "Teet jo joitain juttuja hyvin",
+        "Teet jo paljon hyvää",
         "Olet täydellinen"
     ]
 
-    const [points, setPoints] = useState([50, 50]); // Alussa amat arvot kuin statementListillä
+    const [points, setPoints] = useState([50, 50, 50]); // Alussa amat arvot kuin statementListillä
     const [grade, setGrade] = useState(null);
 
     const handlePointsChange = (index, value) => {
@@ -33,7 +34,7 @@ export default function  Home() {
 
 
     return (
-        <div>
+        <div className="homePage">
             <h1>Hiilineutraali Suomi 2035</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum ultricies sapien, tempor vestibulum risus rutrum non. Praesent aliquam accumsan nulla ut ultrices. Morbi feugiat faucibus justo, sit amet mollis nisl scelerisque id. Cras facilisis tincidunt massa ut mattis. Curabitur non arcu sed lacus varius faucibus. Donec quis maximus eros, vulputate tincidunt enim. Aenean volutpat ullamcorper est, id maximus purus dapibus non. Aenean urna nulla, bibendum id mattis sed, viverra facilisis mauris. Praesent lacinia porttitor blandit. Duis ac rutrum risus, sit amet venenatis ligula. Praesent aliquam venenatis turpis, at vulputate ligula venenatis vel. Nunc erat massa, semper sit amet elit eget, euismod posuere enim. Nunc vestibulum accumsan ligula, et vehicula neque consequat sed. Ut ut elit egestas enim dapibus rhoncus non eu metus. Aliquam erat volutpat.
             Fusce eget lacus eu velit vulputate vulputate. Donec ut ultricies odio. Mauris consequat felis eget lectus vestibulum, ut malesuada magna ultricies. Integer hendrerit lectus urna, at luctus dolor mattis id. Duis consequat mauris sit amet dapibus luctus. Aenean lacinia facilisis ultricies. Maecenas suscipit eget magna a fermentum. Aliquam erat volutpat. Nullam sollicitudin in nibh sed tincidunt. Fusce eleifend magna in libero mollis euismod. Sed eu efficitur ex. Suspendisse facilisis facilisis interdum. Curabitur fermentum rhoncus.
@@ -60,7 +61,7 @@ export default function  Home() {
 
                 {grade && (
                     <div>
-                    <p>Your grade: {feedBack[grade-1]}</p>
+                    <p>{feedBack[grade-1]}</p>
                     </div>
                 )}
                 </div>
--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./slider.css"
 
 export function Slider(props) {
 
     return(
         <div class="slidecontainer">
-            <input 
-            type="range" 
+            <input type="range" 
             min="1" 
             max="100" 
             value={props.value} 
             className="slider"         
-            onChange={(event) => props.onChange(parseInt(event.target.value))}
+            onChange={(event) => props.onChange && props.onChange(parseInt(event.target.value))}
             onMouseUp={props.onAfterChange}
             onTouchEnd={props.onAfterChange}>
             </input>
