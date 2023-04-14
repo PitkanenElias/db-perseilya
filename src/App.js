c01a6b5c119a33d808a373a6eda0db741daa7672
keinokalle <keinokalle@gmail.com>
2023-04-14 12:08:45 +0300

    Muutoksia package.json:iin: oikeat riippuvuudet laitettu ja nyt liveserver toimii selaimessa käskyllä npm run start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
+import "./styles.css";
 
 export default function App() {
+
     return(
         <div className="App">
             <h1>Hiilineutralius 2035</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum laoreet lacus vehicula aliquam. Suspendisse porttitor nisi nec metus lobortis accumsan. Integer sed quam sit amet orci pellentesque gravida. Curabitur eu molestie diam. Nam blandit sodales aliquet. Nullam metus erat, pharetra sed urna iaculis, mattis lobortis leo. Etiam eu dapibus.</p>
         </div>
     );
+    
 }
