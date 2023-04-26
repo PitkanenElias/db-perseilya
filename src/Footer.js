3d95c5046061bebd1de163a0636ea12a486d6bec
pietarivirkkunen <pietari.virkkunen@aalto.fi>
2023-04-26 14:11:59 +0300

    footteri eteenpäin

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,18 @@ import "./footerStyles.css"
 export default function Footer() {
     return (
         <div className="footer">
-            <img className="ministeriöLogo" src={require("./images/ministerioLogo.svg")} alt="Ympäristöministeriö"/>
+            <div className="footerVasen">
+                <div className="someLogot">
+                </div>
+                <div className="someLogot">
+                    <p className="ig">@hiiletonsuomi</p>
+                    <p className="twitter">@hiiletonsuomi</p>
+                    <p className="facebook">@hiiletonsuomi</p>
+                </div>
+            </div>
+            <div className="footerOikea">
+                <img className="ministeriöLogo" src={require("./images/ministerioLogo.svg")} alt="Ympäristöministeriö"/>
+            </div>
         </div>
     )
 };
