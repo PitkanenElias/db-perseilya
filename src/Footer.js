8dcd848632ee177c04985ece4c2424e9539ba0da
keinokalle <keinokalle@gmail.com>
2023-04-26 13:00:40 +0300

    Loin kuvakansion kuvia varten

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,7 @@ import "./footerStyles.css"
 export default function Footer() {
     return (
         <div className="footer">
-            <p>***************************************************</p>
             <h2>Tämä on footteri ja se on jokaisen sivun pohjalla</h2>
-            <p>***************************************************</p>
         </div>
     )
 };
