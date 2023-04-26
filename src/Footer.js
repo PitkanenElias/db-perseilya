1b61791ef6efebcff96e4963920b78cc748ac3c0
keinokalle <keinokalle@gmail.com>
2023-04-26 12:44:34 +0300

    Loin footer.css ja aloin kehittämään sitä

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./footerStyles.css"
 
 export default function Footer() {
     return (
-        <div>
+        <div className="footer">
             <p>***************************************************</p>
             <h2>Tämä on footteri ja se on jokaisen sivun pohjalla</h2>
             <p>***************************************************</p>
