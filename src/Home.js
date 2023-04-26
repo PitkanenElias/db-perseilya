8bad36a3939be316b450f0809ea2bf88d03a36c9
Jonni Hartikainen <jonni.hartikainen@aalto.fi>
2023-04-26 12:43:49 +0300

    slideri

--- src/Home.js
+++ src/Home.js
@@ -1,77 +1,97 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { Slider } from "./Slider/Slider";
-import "./styles.css"
-
+import "./styles.css";
 
 export default function Home() {
+  /**
+   * Home on kampanjan ensimmäinen sivu ja kotisivu. Se sisältää interkatiivisen kyselyn, sekä sen
+   * antaman palautteen.
+   */
 
-    /**
-     * Home on kampanjan ensimmäinen sivu ja kotisivu. Se sisältää interkatiivisen kyselyn, sekä sen
-     * antaman palautteen.
-     */
+  // Tästä alkavat slideriin ja interaktiiviseen kyselyyn liittyvät arvot ja funktiot
+  const statementList = [
+    { statement: "En aja autolla töihin", value: 5 },
+    { statement: "Syön kasvisruokaa", value: 5 },
+    { statement: "En matkusta lentokoneella", value: 5 },
+  ];
+  const feedBack = [
+    "Olet ilmastontuhoaja. Koita parantaa tapasi",
+    "Sinulla olisi vielä paljon parannettavaa",
+    "Olet aika normaali",
+    "Teet jo joitain juttuja hyvin",
+    "Teet jo paljon hyvää",
+    "Olet täydellinen",
+  ];
 
-    // Tästä alkavat slideriin ja interaktiiviseen kyselyyn liittyvät arvot ja funktiot
-    const statementList = [{statement: "En aja autolla töihin", value: 50}, {statement: "Syön kasvisruokaa", value: 50}, {statement: "En matkusta lentokoneella", value: 50}]
-    const feedBack = [
-        "Olet ilmastontuhoaja. Koita parantaa tapasi",
-        "Sinulla olisi vielä paljon parannettavaa",
-        "Olet aika normaali",
-        "Teet jo joitain juttuja hyvin",
-        "Teet jo paljon hyvää",
-        "Olet täydellinen"
-    ]
+  const [points, setPoints] = useState([5, 5, 5]); // Alussa amat arvot kuin statementListillä
+  const [grade, setGrade] = useState(null);
 
-    const [points, setPoints] = useState([50, 50, 50]); // Alussa amat arvot kuin statementListillä
-    const [grade, setGrade] = useState(null);
+  const handlePointsChange = (index, value) => {
+    const newPoints = [...points];
+    newPoints[index] = value;
+    setPoints(newPoints);
+  };
 
-    const handlePointsChange = (index, value) => {
-        const newPoints = [...points];
-        newPoints[index] = value;
-        setPoints(newPoints);
-    };
+  const calculateGrade = () => {
+    const total = points.reduce((acc, cur) => acc + cur, 0);
+    const percentage = total / (points.length * 100);
+    const grade = Math.round(percentage * 5);
+    setGrade(grade);
+  };
 
-    const calculateGrade = () => {
-        const total = points.reduce((acc, cur) => acc + cur, 0);
-        const percentage = total / (points.length * 100);
-        const grade = Math.round(percentage * 5);
-        setGrade(grade);
-    };
+  return (
+    <div className="homePage">
+      <h1 className="otsikko">HIILINEUTRAALI SUOMI 2035</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum
+        ultricies sapien, tempor vestibulum risus rutrum non. Praesent aliquam
+        accumsan nulla ut ultrices. Morbi feugiat faucibus justo, sit amet
+        mollis nisl scelerisque id. Cras facilisis tincidunt massa ut mattis.
+        Curabitur non arcu sed lacus varius faucibus. Donec quis maximus eros,
+        vulputate tincidunt enim. Aenean volutpat ullamcorper est, id maximus
+        purus dapibus non. Aenean urna nulla, bibendum id mattis sed, viverra
+        facilisis mauris. Praesent lacinia porttitor blandit. Duis ac rutrum
+        risus, sit amet venenatis ligula. Praesent aliquam venenatis turpis, at
+        vulputate ligula venenatis vel. Nunc erat massa, semper sit amet elit
+        eget, euismod posuere enim. Nunc vestibulum accumsan ligula, et vehicula
+        neque consequat sed. Ut ut elit egestas enim dapibus rhoncus non eu
+        metus. Aliquam erat volutpat. Fusce eget lacus eu velit vulputate
+        vulputate. Donec ut ultricies odio. Mauris consequat felis eget lectus
+        vestibulum, ut malesuada magna ultricies. Integer hendrerit lectus urna,
+        at luctus dolor mattis id. Duis consequat mauris sit amet dapibus
+        luctus. Aenean lacinia facilisis ultricies. Maecenas suscipit eget magna
+        a fermentum. Aliquam erat volutpat. Nullam sollicitudin in nibh sed
+        tincidunt. Fusce eleifend magna in libero mollis euismod. Sed eu
+        efficitur ex. Suspendisse facilisis facilisis interdum. Curabitur
+        fermentum rhoncus.
+      </p>
+      <div>
+        <h2>Testaa kuinka paha ilmastotuholainen oletkaan</h2>
+        <div>
+          {statementList.map((statement, index) => {
+            return (
+              <div key={index}>
+                <p>{statement.statement}</p>
+                <Slider
+                  value={points[index]}
+                  min={0}
+                  max={100}
+                  step={1}
+                  onChange={(value) => handlePointsChange(index, value)}
+                  onAfterChange={calculateGrade}
+                />
+                <label>{points[index]}</label>
+              </div>
+            );
+          })}
 
-
-    return (
-        <div className="homePage">
-            <h1 className="otsikko" >HIILINEUTRAALI SUOMI 2035</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum ultricies sapien, tempor vestibulum risus rutrum non. Praesent aliquam accumsan nulla ut ultrices. Morbi feugiat faucibus justo, sit amet mollis nisl scelerisque id. Cras facilisis tincidunt massa ut mattis. Curabitur non arcu sed lacus varius faucibus. Donec quis maximus eros, vulputate tincidunt enim. Aenean volutpat ullamcorper est, id maximus purus dapibus non. Aenean urna nulla, bibendum id mattis sed, viverra facilisis mauris. Praesent lacinia porttitor blandit. Duis ac rutrum risus, sit amet venenatis ligula. Praesent aliquam venenatis turpis, at vulputate ligula venenatis vel. Nunc erat massa, semper sit amet elit eget, euismod posuere enim. Nunc vestibulum accumsan ligula, et vehicula neque consequat sed. Ut ut elit egestas enim dapibus rhoncus non eu metus. Aliquam erat volutpat.
-            Fusce eget lacus eu velit vulputate vulputate. Donec ut ultricies odio. Mauris consequat felis eget lectus vestibulum, ut malesuada magna ultricies. Integer hendrerit lectus urna, at luctus dolor mattis id. Duis consequat mauris sit amet dapibus luctus. Aenean lacinia facilisis ultricies. Maecenas suscipit eget magna a fermentum. Aliquam erat volutpat. Nullam sollicitudin in nibh sed tincidunt. Fusce eleifend magna in libero mollis euismod. Sed eu efficitur ex. Suspendisse facilisis facilisis interdum. Curabitur fermentum rhoncus.
-            </p>
+          {(grade || grade === 0) && (
             <div>
-                <h2>Testaa kuinka paha ilmastotuholainen oletkaan</h2>
-                <div>
-                {statementList.map((statement, index) => {
-                    return(
-                        <div key={index}>
-                        <p>{statement.statement}</p>
-                        <Slider
-                            value={points[index]}
-                            min={0}
-                            max={100}
-                            step={1}
-                            onChange={(value) => handlePointsChange(index, value)}
-                            onAfterChange={calculateGrade}
-                        />
-                        <label>{points[index]}</label>
-                        </div>
-                    )
-                })}
-
-                {(grade || grade===0) && (
-                    <div>
-                    <h2>{feedBack[grade]}</h2>
-                    </div>
-                )}
-                </div>
+              <h2>{feedBack[grade]}</h2>
             </div>
+          )}
         </div>
-        
-    );
-};
+      </div>
+    </div>
+  );
+}
--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import "./slider.css"
+import "./slider.css";
 
 export function Slider(props) {
-
-    return(
-        <div class="slidecontainer">
-            <input type="range" 
-            min="0" 
-            max="100" 
-            step="25"
-            value={props.value} 
-            className="slider"         
-            onChange={(event) => props.onChange && props.onChange(parseInt(event.target.value))}
-            onMouseUp={props.onAfterChange}
-            onTouchEnd={props.onAfterChange}>
-            </input>
-        </div>
-    );
-};
+  return (
+    <div class="slidecontainer">
+      <input
+        type="range"
+        min="0"
+        max="10"
+        step="1"
+        value={props.value}
+        className="slider"
+        onChange={(event) =>
+          props.onChange && props.onChange(parseInt(event.target.value))
+        }
+        onMouseUp={props.onAfterChange}
+        onTouchEnd={props.onAfterChange}
+      ></input>
+    </div>
+  );
+}
