d0fa313ac6eae47171ca3f2464b552916e4344ba
Jonni Hartikainen <jonni.hartikainen@aalto.fi>
2023-04-28 13:25:09 +0300

    lisätty about.css

--- src/About.js
+++ src/About.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React from "react";
+import "./about.css";
 
 export default function Info() {
-    return (
-        <div>
-            <h2>Informaatiota hiilineutraaliudesta</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum laoreet lacus vehicula aliquam. Suspendisse porttitor nisi nec metus lobortis accumsan. Integer sed quam sit amet orci pellentesque gravida. Curabitur eu molestie diam. Nam blandit sodales aliquet. Nullam metus erat, pharetra sed urna iaculis, mattis lobortis leo. Etiam eu dapibus.</p>
-        </div>
-    )
-};
+  return (
+    <div className="kontentti">
+      <h2 className="pääotsikko">Informaatiota hiilineutraaliudesta</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum
+        laoreet lacus vehicula aliquam. Suspendisse porttitor nisi nec metus
+        lobortis accumsan. Integer sed quam sit amet orci pellentesque gravida.
+        Curabitur eu molestie diam. Nam blandit sodales aliquet. Nullam metus
+        erat, pharetra sed urna iaculis, mattis lobortis leo. Etiam eu dapibus.
+      </p>
+    </div>
+  );
+}
