691fd4698606656e21859d0393297692697aeabd
mtelov3 <mtelov3@gmail.com>
2023-05-03 13:26:10 +0300

    Git lagailee

--- src/About.js
+++ src/About.js
@@ -1,10 +1,13 @@
 import React from "react"
+import"./about.css";
 
 export default function Info() {
     return (
         <div>
-            <h2>Informaatiota hiilineutraaliudesta</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum laoreet lacus vehicula aliquam. Suspendisse porttitor nisi nec metus lobortis accumsan. Integer sed quam sit amet orci pellentesque gravida. Curabitur eu molestie diam. Nam blandit sodales aliquet. Nullam metus erat, pharetra sed urna iaculis, mattis lobortis leo. Etiam eu dapibus.</p>
+            <h2 className="otsikko"> Informaatiota hiilineutraaliudesta</h2>
+            <p data-tooltip="Tekstiä tänne"></p>
+
+
         </div>
     )
 };
