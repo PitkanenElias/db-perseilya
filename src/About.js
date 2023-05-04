2780ceba71c77e899ad7f323bce1fa0b77497180
mtelov3 <mtelov3@gmail.com>
2023-05-04 15:56:02 +0300

    Meistä sivulle konkrteiaa

--- src/About.js
+++ src/About.js
@@ -2,16 +2,84 @@ import React from "react";
 import "./about.css";
 
 export default function Info() {
-  return (
-    <div className="kontentti">
-      <h2 className="pääotsikko">Informaatiota hiilineutraaliudesta</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum
-        laoreet lacus vehicula aliquam. Suspendisse porttitor nisi nec metus
-        lobortis accumsan. Integer sed quam sit amet orci pellentesque gravida.
-        Curabitur eu molestie diam. Nam blandit sodales aliquet. Nullam metus
-        erat, pharetra sed urna iaculis, mattis lobortis leo. Etiam eu dapibus.
-      </p>
-    </div>
+  return ( 
+  <><head>
+      <title>Circle Image Website</title>
+      <link rel="stylesheet" type="text/css" href="styles.css" />
+    </head><body>
+        <section class="grid-container">
+          <div class="timeline-text">
+            <p>
+              Vuoden 2022 marraskuussa järjestetyn Egyptin ilmastokokouksen
+              jälkimainingeissa nousee esiin ajatus Suomesta ilmastotoimien
+              johtomaana. Virkamiestyöstö alkaa kansallisen kampanjan
+              aikaansaamiseksi.
+            </p>
+          </div>
+          <div class="timeline-photo">
+            <img src="./content/un-flags.png" alt="united nations" />
+          </div>
+          <div class="timeline-photo">
+            <img src="./content/finnish-parliament.png" alt="finnish parliament" />
+          </div>
+          <div class="timeline-text">
+            <p>
+              Toukokuussa 2023 hallitus linjaa hallitusohjelmassaan tavoitteen
+              siitä, että ilmastoteoilla, innovaatioilla ja osaamisella Suomen
+              ilmastopolitiikasta rakennetaan vientituote. Ensimmäiset visiot
+              kansallisen kampanjan muodosta alkavat hahmottua. Tässä kohtaa
+              kampanja kulkee vain työnimellä “Tulevaisuushanke”.
+            </p>
+          </div>
+          <div class="timeline-text">
+            <p>
+              Tammikuussa 2024 alkaa Hiilineutraali Suomi 2035 -kampanjan
+              materiaalien levitys mediassa. Kyseessä on kunnianhimoinen kampanja,
+              jonka tavoitteet pyrkivät aikaansaamaan 1920-luvun yhteiskunnan uusien
+              tuulien kaltaisia asenteenmuutoksia.
+            </p>
+          </div>
+          <div class="timeline-photo">
+            <img src="./content/linda-eyeroll.png" alt="eyeroll" />
+          </div>
+          <div class="timeline-photo">
+            <img src="./content/linda-eyeroll.png" alt="eyeroll" />
+          </div>
+          <div class="timeline-text">
+            <p>
+              Kampanjan toteutusaika on vuosien 2025 ja 2035 välillä ja se pyrkii
+              ulottumaan jokaiseen suomalaisen yhteiskunnan lokeroon. Tarkastelet
+              tällä hetkellä kampanjan kansalaisille suunnattua sivustoa, joka
+              pureutuu jokapäiväiseen kuluttamiseen. Juuri sinä oletkin
+              avainasemassa Suomen aseman kirjoittamisessa historiankirjoihin.
+              Jäätkö odottamaan, vai aiotko uskaltaa?
+            </p>
+          </div>
+          <div class="timeline-text">
+            <p>
+              Kampanja ei saavuta tavoitteitaan, ja Suomi päättää kollektiivisesti
+              ja tietoisesti jäädä peesaamaan tulevaisuuskehityksen suhteen.
+              Joudumme tanssimaan kehitysvastuun ottaneiden maiden pillien mukaan,
+              kun yhteiskuntaamme järkyttää ulkopuolinen paine - vaikka meille
+              puhdasta vettä ja laidunmaata riittääkin, poliittisesti voimakkaammat
+              maat ja paremman neuvotteluaseman saavuttaneet tahot joutuvat tekemään
+              meistä riippumattomia päätöksiä siitä, minne miljardit ihmiset
+              siirretään ja heidän tarpeidensa tyydyttämiseen vaadittavat resurssit
+              luodaan.
+            </p>
+          </div>
+          <div class="timeline-text">
+            <p>
+              Suomi on hiilineutraali jo reippaasti ennen muuta Eurooppaa, saatika
+              muuta maailmaa. Suomalainen luontoystävällinen elämäntapa on suosiossa
+              maailmalla, ja maatamme kohtaan suhtaudutaan kunnioituksella ja
+              ihainnoinnilla. Suomalaiset yritykset kulkevat kehityksen
+              aallonharjalla ja luovat hyvinvointia valtakuntaan työpaikkojen,
+              patenttiteknologian ja kestävän talouskasvun muodossa.
+            </p>
+          </div>
+        </section>
+      </body></>
+    
   );
 }
