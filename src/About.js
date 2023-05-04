78b5f86bd85b42cc296ec19965c00c2b39ffd906
Elias Pitkänen <2elias.pitkanen9@gmail.com>
2023-05-05 00:02:36 +0300

    päivitetty about.js html-rakenne

--- src/About.js
+++ src/About.js
@@ -2,14 +2,24 @@ import React from "react";
 import "./about.css";
 
 export default function Info() {
-  return ( 
-  <><head>
-      <title>Circle Image Website</title>
-      <link rel="stylesheet" type="text/css" href="styles.css" />
-    </head>
-     <body>
+  return (
+    <>
+      <head>
+        <title>Circle Image Website</title>
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
         <section class="grid-container">
-          <div class="timeline-text">
+          <div class="bg-path">
+            <img src="/content/line.svg" alt="background line" />
+          </div>
+          <div class="timeline-el">
             <p>
               Vuoden 2022 marraskuussa järjestetyn Egyptin ilmastokokouksen
               jälkimainingeissa nousee esiin ajatus Suomesta ilmastotoimien
@@ -17,13 +27,16 @@ export default function Info() {
               aikaansaamiseksi.
             </p>
           </div>
-          <div class="timeline-photo">
-            <img  alt="united nations" />
+          <div class="timeline-el">
+            <img src="./content/un-flags.png" alt="united nations" />
           </div>
-          <div class="timeline-photo">
-            <img alt="finnish parliament" />
+          <div class="timeline-el">
+            <img
+              src="./content/finnish-parliament.png"
+              alt="finnish parliament"
+            />
           </div>
-          <div class="timeline-text">
+          <div class="timeline-el">
             <p>
               Toukokuussa 2023 hallitus linjaa hallitusohjelmassaan tavoitteen
               siitä, että ilmastoteoilla, innovaatioilla ja osaamisella Suomen
@@ -32,55 +45,57 @@ export default function Info() {
               kampanja kulkee vain työnimellä “Tulevaisuushanke”.
             </p>
           </div>
-          <div class="timeline-text">
+          <div class="timeline-el">
             <p>
               Tammikuussa 2024 alkaa Hiilineutraali Suomi 2035 -kampanjan
-              materiaalien levitys mediassa. Kyseessä on kunnianhimoinen kampanja,
-              jonka tavoitteet pyrkivät aikaansaamaan 1920-luvun yhteiskunnan uusien
-              tuulien kaltaisia asenteenmuutoksia.
+              materiaalien levitys mediassa. Kyseessä on kunnianhimoinen
+              kampanja, jonka tavoitteet pyrkivät aikaansaamaan 1920-luvun
+              yhteiskunnan uusien tuulien kaltaisia asenteenmuutoksia.
             </p>
           </div>
-          <div class="timeline-photo">
-            <img  alt="eyeroll" />
+          <div class="timeline-el">
+            <img src="./content/social-media.png" alt="social media" />
           </div>
-          <div class="timeline-photo">
-            <img  alt="eyeroll" />
+          <div class="timeline-el">
+            <img src="./content/crosswalk.png" alt="crosswalk people" />
           </div>
-          <div class="timeline-text">
+          <div class="timeline-el">
             <p>
-              Kampanjan toteutusaika on vuosien 2025 ja 2035 välillä ja se pyrkii
-              ulottumaan jokaiseen suomalaisen yhteiskunnan lokeroon. Tarkastelet
-              tällä hetkellä kampanjan kansalaisille suunnattua sivustoa, joka
-              pureutuu jokapäiväiseen kuluttamiseen. Juuri sinä oletkin
-              avainasemassa Suomen aseman kirjoittamisessa historiankirjoihin.
-              Jäätkö odottamaan, vai aiotko uskaltaa?
+              Kampanjan toteutusaika on vuosien 2025 ja 2035 välillä ja se
+              pyrkii ulottumaan jokaiseen suomalaisen yhteiskunnan lokeroon.
+              Tarkastelet tällä hetkellä kampanjan kansalaisille suunnattua
+              sivustoa, joka pureutuu jokapäiväiseen kuluttamiseen. Juuri sinä
+              oletkin avainasemassa Suomen aseman kirjoittamisessa
+              historiankirjoihin. Jäätkö odottamaan, vai aiotko uskaltaa?
             </p>
           </div>
-          <div class="timeline-text">
+          <div class="timeline-el">
             <p>
-              Kampanja ei saavuta tavoitteitaan, ja Suomi päättää kollektiivisesti
-              ja tietoisesti jäädä peesaamaan tulevaisuuskehityksen suhteen.
-              Joudumme tanssimaan kehitysvastuun ottaneiden maiden pillien mukaan,
-              kun yhteiskuntaamme järkyttää ulkopuolinen paine - vaikka meille
-              puhdasta vettä ja laidunmaata riittääkin, poliittisesti voimakkaammat
-              maat ja paremman neuvotteluaseman saavuttaneet tahot joutuvat tekemään
-              meistä riippumattomia päätöksiä siitä, minne miljardit ihmiset
-              siirretään ja heidän tarpeidensa tyydyttämiseen vaadittavat resurssit
+              Kampanja ei saavuta tavoitteitaan, ja Suomi päättää
+              kollektiivisesti ja tietoisesti jäädä peesaamaan
+              tulevaisuuskehityksen suhteen. Joudumme tanssimaan kehitysvastuun
+              ottaneiden maiden pillien mukaan, kun yhteiskuntaamme järkyttää
+              ulkopuolinen paine - vaikka meille puhdasta vettä ja laidunmaata
+              riittääkin, poliittisesti voimakkaammat maat ja paremman
+              neuvotteluaseman saavuttaneet tahot joutuvat tekemään meistä
+              riippumattomia päätöksiä siitä, minne miljardit ihmiset siirretään
+              ja heidän tarpeidensa tyydyttämiseen vaadittavat resurssit
               luodaan.
             </p>
           </div>
-          <div class="timeline-text">
+          <div class="timeline-el">
             <p>
-              Suomi on hiilineutraali jo reippaasti ennen muuta Eurooppaa, saatika
-              muuta maailmaa. Suomalainen luontoystävällinen elämäntapa on suosiossa
-              maailmalla, ja maatamme kohtaan suhtaudutaan kunnioituksella ja
-              ihainnoinnilla. Suomalaiset yritykset kulkevat kehityksen
-              aallonharjalla ja luovat hyvinvointia valtakuntaan työpaikkojen,
-              patenttiteknologian ja kestävän talouskasvun muodossa.
+              Suomi on hiilineutraali jo reippaasti ennen muuta Eurooppaa,
+              saatika muuta maailmaa. Suomalainen luontoystävällinen elämäntapa
+              on suosiossa maailmalla, ja maatamme kohtaan suhtaudutaan
+              kunnioituksella ja ihainnoinnilla. Suomalaiset yritykset kulkevat
+              kehityksen aallonharjalla ja luovat hyvinvointia valtakuntaan
+              työpaikkojen, patenttiteknologian ja kestävän talouskasvun
+              muodossa.
             </p>
           </div>
         </section>
-      </body></>
-    
+      </body>
+    </>
   );
 }
