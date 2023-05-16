1f6d5a5f3991a7a43882eaeb1b14f124fe4bdc10
keinokalle <keinokalle@gmail.com>
2023-05-16 23:35:12 +0300

    turhaa hienosäätöä

--- src/Home.js
+++ src/Home.js
@@ -141,8 +141,8 @@ export default function Home() {
     if(deers && metro){
       window.addEventListener('scroll', () => {
         let x = window.pageYOffset;
-        deers.style.transform = `translateX(${(x/9)-70}px)`;
-        metro.style.transform = `translateX(${-(x/9)+70}px)`;
+        metro.style.transform = `translateX(${-(x/9)+50}px)`;
+        deers.style.transform = `translateX(${(x/9)-110}px)`;
       });
     }
   }, [deers, metro]);
