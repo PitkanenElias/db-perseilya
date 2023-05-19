ec0daaa174f815f505c00f3acb1cfe10378a158a
keinokalle <keinokalle@gmail.com>
2023-05-19 16:36:41 +0300

    (ehkä) lopullinen tietoa sivu

--- src/Tietoa.js
+++ src/Tietoa.js
@@ -29,9 +29,6 @@ export default function About() {
         </div>  
         <div id="menuColor"></div>
         <div className="asiaa">
-          <h3 className="toinenOtsikko">
-            Lisätietoa Hiilineutraali Suomi 2035-hankkeesta
-          </h3>
           <p className="tietoTekstiä">
           72 prosenttia koko maailman kasvihuonekaasupäästöistä aiheutuu kotitalouksien kulutuksesta. Myös Suomessa kotitalouksien kulutus muodostaa 68 prosenttia kotimaan kasvihuonekaasupäästöistä. Loput syntyvät julkisesta kulutuksesta ja investoinneista. Ilmastonmuutoksen hillintä edellyttääkin siis toimia koko yhteiskunnassa. <br/><br/>
           Vuonna 2010 suomalaisen kulutusmenoista keskimäärin aiheutuvat kasvihuonekaasupäästöt olivat 11,5 tCO2e / henkilö. Koko maailman keskiarvo oli 6,0 tCO2e,  eli suomalaisten päästöt ovat tähän verrattuna lähes kaksinkertaiset. Myös Euroopan mittakaavassa päästömäärä on korkea, ja yhteisvastuun nimissä suomalaisten on aika toimia.<br/><br/>
@@ -40,57 +37,58 @@ export default function About() {
           </p>
           <h3 className="väliOtsikko">Arkielämän osa-alueita:</h3>
           <div className="osa-alueet">
-          <div className="arkielamaa">
-            <h4 className="väliväliOtsikko">Vedestä:</h4>
-            <p className="esittelyTeksti">
-            Turha veden juoksutus, liian pitkät suihkut ja vuotavat vesikalusteet voi huomaamatta valuttaa hukkaan kymmeniä litroja vettä päivässä. Vähentämällä ylimääräistä veden kulutusta, voit sekä keventää ympäristön kuormaa että säästää omaa lompakkoasi.
-            <br/><br/>Kahdeksan minuutin suihku joka päivä maksaa sinulle vuodessa noin 300 euroa. Puolittamalla suihkussa käytetyn ajan puolitat samalla vedestä maksamasi laskun. Lämmin vesi maksaa keskimäärin yli kaksi kertaa enemmän kuin kylmä vesi. Puolittamalla suihkuajan kahdeksasta minuutista neljään minuuttiin hiilijalanjälkesi pienenee 0,5 % vuodessa. Asentamalla vesimittarin ja säästämällä 20 % vedenkulutuksesta pienennät hiilijalanjälkeäsi toisella 0,5 % vuodessa. Jos miljoona suomalaista tekisi näin, säästyisi lähes 9 000 henkilön kokonaishiilijalanjälki.
-            </p>
-            <img className="icon1"
-            src={require("./images/Vesivuoto.svg")}
-            alt="Vesihana"> 
-            </img>
-          </div>
-          <div className="arkielamaa">
-            <h4 className="väliväliOtsikko">Energiasta:</h4>
-            <p className="esittelyTeksti">
-            Kodeissa energiaa kuluu eniten lämmitykseen. Toiseksi eniten sähköä vie veden lämmittäminen. Onkin erityisen tärkeää varmistaa, että asunnon lämpötila on korkeintaan 21 astetta ja lämmintä vettä ei tuhlata.
-            <br/><br/>
-            Kodin energiatehokkuutta parantavista ratkaisuista ilmalämpöpumppu on kustannustehokkain. Talvella se lämmittää kodin tehokkaasti ja edullisesti, kun taas helteillä se jäähdyttää huoneilmaa. Ilmalämpöpumpun avulla hiilijalanjälkesi pienenee 4 % vuodessa. Jos miljoona suomalaista asentaisi ilmalämpöpumpun, säästyisi noin 39 000 henkilön kokonaishiilijalanjälki.
-            <br/><br/>
-            Kun asennat omakotitalon tai mökin katolle aurinkokeräimen, saat lämmintä käyttövettä ilmaiseksi. Asennettua järjestelmää tarvitsee huoltaa vain harvoin. Alkuinvestoinnin jälkeen säästöä syntyy muun muassa lämmityskustannuksissa ja hiilijalanjälkesi pienenee 7 % vuodessa. Jos miljoonaa suomalaista asentaisi keräimen, säästyisi 66 000 asukkaan kokonaishiilijalanjälki.
-            </p>
-            <img className="icon1"
-            src={require("./images/Sähkö.svg")}
-            alt="Sähkö"> 
-            </img>
-          </div>
-          <div className="arkielamaa">
-            <h4 className="väliväliOtsikko">Kulkemisesta:</h4>
-            <p className="esittelyTeksti">
-            Liikenne tuottaa noin 20 prosenttia Suomen kasvihuonekaasupäästöistä. Onneksi bensiinille on nykyään vähäpäästöisempiä vaihtoehtoja. Yksi näistä on bioetanoli, joka on biojätteestä valmistettu kotimainen ja ympäristöystävällinen valinta. Suomessa RE85-etanolipolttoainetta valmistetaan tähteistä, kuten hävikkileivästä. Pienellä investoinnilla voit muuttaa bensiini- tai dieselkäyttöisen auton RE85 käyttöiseksi, kunhan autossa ei ole suorasuihkumoottoria. Biojätteestä valmistetun etanolin osuus polttonesteessä laskee autoilun fossiilisia päästöjä jopa 80 prosenttia. Etanolikäyttöisellä autolla ajaminen polttomoottoriauton sijaan pienentää hiilijalanjälkeäsi 16,5 prosenttia vuodessa. Jos miljoonaa suomalaista tekisi näin, laskennallisesti säästyisi noin 181 000 asukkaan kokonaishiilijalanjälki.
-            <br/><br/>
-            Tai mitä jos pyytäisit kaverin kyytiin tai perustaisit kimppakyytiryhmän? Jaettu työmatka on myös oiva hetki aamupalaverille työporukan kesken. Jos matkustat kolmen henkilön kimppakyydissä kolmesti viikossa yksin ajamisen sijasta, pienenee hiilijalanjälkesi 1 % vuodessa. Jos miljoona suomalaista tekee näin, säästyy 11 000 suomalaisen hiilijalanjälki vuodessa.
-            <br/><br/>
-            Toisaalta etätyöpäivänä työt voit tehdä kotoa tai vaikkapa kahvilasta käsin. Työmatkoihin kuluva aika jää muuhun, ja joidenkin töiden tekeminen on tehokkaampaa. Jos työpaikallasi ei ole ollut tapana tehdä etätöitä, saattaa koko käsite olla työkavereiden keskuudessa vieras. Yksi etätyöpäivä viikossa pienentää hiilijalanjälkeä prosentin. Jos miljoona suomalaista tekee lisäksesi yhden etäpäivän viikossa, säästyy 11 000 henkilön hiilijalanjälki vuodessa.
-            </p>
-            <img className="icon1"
-            src={require("./images/Auto.svg")}
-            alt="Auto"> 
-            </img>
-          </div>
-          <div className="arkielamaa">
-            <h4 className="väliväliOtsikko">Hankinnoista</h4>
-            <p className="esittelyTeksti">
-            Haasta itsesi testaamaan kasvisruokavaliota vuoden ajan. Luvassa on taattuja hyötyjä. Ruokavalio monipuolistuu ja kokkaustaidot karttuvat. Kasvisruoka on edullista, terveellistä ja herkullista. Yhä useampi meistä on kasvissyöjä, joten valikoimaa ja vertaistukea on tarjolla runsaasti. Lihasta luopumalla voit pienentää hiilijalanjälkeäsi seitsemän prosenttia vuodessa. Säästö vastaa esimerkiksi 4100 autoilukilometriä.
-            <br/><br/>
-            Vanhat aarniometsät ovat tärkeitä hiilivarastoja ja yhteistä perintöämme, jonka suojelijaksi jokainen voi ryhtyä. Lahjoittamalla esimerkiksi sata euroa vuosittain ikimetsien suojeluun varmistat, että alueet muuttuvat luonnonsuojelualueiksi, jotka varastoivat hiiltä sekä ylläpitävät lajien kirjoa. Sadalla eurolla Luonnonperintösäätiö ostaa 200 neliömetriä luonnonalueita ja rauhoittaa ne pysyvästi.            </p>
-            <img className="icon1"
-            src={require("./images/Ostoskassi.svg")}
-            alt="Ostoskassi"> 
-            </img>
+            <div className="arkielamaa">
+              <h4 className="väliväliOtsikko">Vedestä:</h4>
+              <p className="esittelyTeksti">
+              Turha veden juoksutus, liian pitkät suihkut ja vuotavat vesikalusteet voi huomaamatta valuttaa hukkaan kymmeniä litroja vettä päivässä. Vähentämällä ylimääräistä veden kulutusta, voit sekä keventää ympäristön kuormaa että säästää omaa lompakkoasi.
+              <br/><br/>Kahdeksan minuutin päivittäinen suihku maksaa vuodessa noin 300 euroa. Puolittamalla ajan puolitat samalla vesilaskun. Samalla hiilijalanjälkesi pienenee 0,5 % vuodessa. Tämän lisäksi asentamalla vesimittarin ja säästämällä 20 % vedenkulutuksesta pienennät hiilijalanjälkeäsi toisella 0,5 %:lla  vuodessa. Jos miljoona suomalaista tekisi näin, säästyisi lähes 9 000 henkilön kokonaishiilijalanjälki.
+              </p>
+              <img className="icon1"
+              src={require("./images/Vesivuoto.svg")}
+              alt="Vesihana"> 
+              </img>
             </div>
-          </div>
+            <div className="arkielamaa">
+              <h4 className="väliväliOtsikko">Energiasta:</h4>
+              <p className="esittelyTeksti">
+              Kodeissa energiaa kuluu eniten lämmitykseen. Toiseksi eniten sähköä vie veden lämmittäminen. Onkin erityisen tärkeää varmistaa, että asunnon lämpötila on korkeintaan 21 astetta ja lämmintä vettä ei tuhlata.
+              <br/><br/>
+              Kodin energiatehokkuutta parantavista ratkaisuista ilmalämpöpumppu on kustannustehokkain. Ilmalämpöpumpun avulla hiilijalanjälkesi pienenee 4 % vuodessa. Jos miljoona suomalaista asentaisi ilmalämpöpumpun, säästyisi noin 39 000 henkilön kokonaishiilijalanjälki.
+              <br/><br/>
+              Asentamalla omakotitalon tai mökin katolle aurinkokeräimen saat lämmintä käyttövettä ilmaiseksi, ja järjestelmää tarvitsee huoltaa vain harvoin. Alkuinvestoinnin jälkeen säästöä syntyy muun muassa lämmityskustannuksista ja hiilijalanjälkesi pienenee 7 % vuodessa. Jos miljoonaa suomalaista asentaisi keräimen, säästyisi 66 000 asukkaan kokonaishiilijalanjälki.
+              </p>
+              <img className="icon1"
+              src={require("./images/Sähkö.svg")}
+              alt="Sähkö"> 
+              </img>
+            </div>
+            <div className="arkielamaa">
+              <h4 className="väliväliOtsikko">Kulkemisesta:</h4>
+              <p className="esittelyTeksti">
+              Liikenne tuottaa noin 20 prosenttia Suomen kasvihuonekaasupäästöistä. Pienellä investoinnilla voit muuttaa bensiini- tai dieselkäyttöisen auton bioetanolikäyttöiseksi, kunhan autossa ei ole suorasuihkumoottoria. Bioetanolin osuus polttonesteessä laskee autoilun fossiilisia päästöjä jopa 80 prosenttia ja pienentää hiilijalanjälkeäsi 16,5 prosenttia vuodessa. Laskennallisesti, jos miljoonaa suomalaista tekisi näin, säästyisi noin 181 000 asukkaan kokonaishiilijalanjälki.
+              <br/><br/>
+              Toisaalta jaettu työmatka on myös oiva hetki aamupalaverille työporukan kesken. Jos matkustat kolmen henkilön kimppakyydissä kolmesti viikossa yksin ajamisen sijasta, pienenee hiilijalanjälkesi 1 % vuodessa. Jos miljoona suomalaista tekeisi näin, säästyisi 11 000 asukkaan kokonaishiilijalanjälki.
+              <br/><br/>
+              Tämän lisäksi etätyöpäivänä työt voit tehdä kotoa tai vaikkapa kahvilasta käsin ja työmatkoihin kuluva aika jää muuhun. Jos työpaikallasi ei ole ollut tapana tehdä etätöitä, saattaa koko käsite olla työkavereiden keskuudessa vieras. Yksi etätyöpäivä viikossa pienentää hiilijalanjälkeäsi prosentilla. Jos miljoona suomalaista tekee lisäksesi yhden etäpäivän viikossa, säästyisi 11 000 asukkaan kokonaishiilijalanjälki.
+              </p>
+              <img className="icon1"
+              src={require("./images/Auto.svg")}
+              alt="Auto"> 
+              </img>
+            </div>
+            <div className="arkielamaa">
+              <h4 className="väliväliOtsikko">Hankinnoista</h4>
+              <p className="esittelyTeksti">
+                Entä, jos haastaisit itsesi testaamaan kasvisruokavaliota vuoden ajan? Ruokavalio monipuolistuu, kokkaustaidot karttuvat ja kasvisruoka on edullista, terveellistä ja herkullista. Yhä useampi meistä on kasvissyöjä, joten valikoimaa ja vertaistukea on tarjolla runsaasti. Lihasta luopumalla voit pienentää hiilijalanjälkeäsi 7 % vuodessa. Säästö vastaa esimerkiksi 4100 autoilukilometriä.
+                <br/><br/>
+                Kiinnostaisiko oikea voittajan peliliike? Vanhat aarniometsät ovat tärkeitä hiilivarastoja ja yhteistä perintöämme, jonka suojelijaksi jokainen voi ryhtyä. Lahjoittamalla esimerkiksi sata euroa vuosittain ikimetsien suojeluun varmistat, että alueet muuttuvat luonnonsuojelualueiksi, jotka varastoivat hiiltä sekä ylläpitävät lajien kirjoa. Sadalla eurolla Luonnonperintösäätiö ostaa 200 neliömetriä luonnonalueita ja rauhoittaa ne pysyvästi.
+              </p>
+              <img className="icon1"
+              src={require("./images/Ostoskassi.svg")}
+              alt="Ostoskassi"> 
+              </img>
+            </div>
+         </div>
         </div>
         <h3 className="väliOtsikko">Ulkopuolisia lähteitä</h3>
         <div className="wrappaaja">
