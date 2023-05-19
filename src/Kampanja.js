2269b9cb578f3a054baa855a0b3e854b3d80da26
keinokalle <keinokalle@gmail.com>
2023-05-19 17:37:40 +0300

    lisäsin eliaksen lisäykset kampanjasivulle

--- src/Kampanja.js
+++ src/Kampanja.js
@@ -13,118 +13,182 @@ export default function Info() {
       behavior: "instant"
     });
   }, []);
+
+  var oddChildren = document.querySelectorAll('.timeline-el:nth-child(odd)');
+  var evenChildren = document.querySelectorAll('.timeline-el:nth-child(even)');
+
+  function applyRandomOffset(elements) {
+    elements.forEach(function (element) {
+      var randomTranslateY = Math.floor(Math.random() * 11) - 5; // Random number between -25 and 25
+
+      if (elements === oddChildren) {
+        var randomTranslateX = Math.floor(Math.random() * 5); // Random number between 0 and 25
+      } else if (elements === evenChildren) {
+        var randomTranslateX = Math.floor(Math.random() * -5); // Random number between -25 and 0
+      }
+
+      element.style.transform =
+        'translate(' + randomTranslateX + 'rem, ' + randomTranslateY + 'rem)';
+    });
+  }
+
+  applyRandomOffset(oddChildren);
+  applyRandomOffset(evenChildren);
   
   return (
     <>
-      <div id="menuColor"></div>
-      <head>
-        <title>Circle Image Website</title>
-        <link rel="stylesheet" type="text/css" href="about.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <section class="grid-container">
-          <div class="bg-path">
-            <img src={require("./images/line.svg")} alt="katkoviiva" />
+    <body>
+        <header className="intro-block">
+          <img src={require("./images/about-header.jpg")} alt="taiga stream header" />
+          <div className="headers">
+            <h1 className="bigHeader">TIETOA</h1>
+            <h1 className="bigHeader">KAMPANJASTA</h1>
           </div>
-          <div class="timeline-el">
-            <p>
-              Vuoden 2022 marraskuussa järjestetyn Egyptin ilmastokokouksen
-              jälkimainingeissa nousee esiin ajatus Suomesta ilmastotoimien
-              johtomaana. Virkamiestyöstö alkaa kansallisen kampanjan
-              aikaansaamiseksi.
-            </p>
+          <div className="intro-text">
+            <h2>
+              Tämäkin kampanja on kulkenut pitkän matkan, ennen kuin se on päätynyt
+              katsottavaksesi. Tältä sivulta voit perehtyä menneeseen ja
+              vaihtoehtoisiin tuleviin.
+            </h2>
           </div>
-          <div class="timeline-el">
-            <img
-              src={require("./images/webp/un-flags.webp")}
-              alt="YK-maiden lippuja"
-            ></img>
-          </div>
-          <div class="timeline-el">
-            <img
-              src={require("./images/webp/finnish-parliament.webp")}
-              alt="tuoleja istuntosalissa"
-            />
-          </div>
-          <div class="timeline-el">
-            <p>
-              Toukokuussa 2023 hallitus linjaa hallitusohjelmassaan tavoitteen
-              siitä, että ilmastoteoilla, innovaatioilla ja osaamisella Suomen
-              ilmastopolitiikasta rakennetaan vientituote. Ensimmäiset visiot
-              kansallisen kampanjan muodosta alkavat hahmottua. Tässä kohtaa
-              kampanja kulkee vain työnimellä “Tulevaisuushanke”.
-            </p>
-          </div>
-          <div class="timeline-el">
-            <p>
-              Tammikuussa 2024 alkaa Hiilineutraali Suomi 2035 -kampanjan
-              materiaalien levitys mediassa. Kyseessä on kunnianhimoinen
-              kampanja, jonka tavoitteet pyrkivät aikaansaamaan 1920-luvun
-              yhteiskunnan uusien tuulien kaltaisia asenteenmuutoksia.
-            </p>
-          </div>
-          <div class="timeline-el">
-            <img
-              src={require("./images/webp/social-media.webp")}
-              alt="some-tykkäys"
-            />
-          </div>
-          <div class="timeline-el">
-            <img
-              src={require("./images/webp/crosswalk.webp")}
-              alt="ihmisiä ylittämässä suojatietä"
-            />
-          </div>
-          <div class="timeline-el">
-            <p>
-              Kampanjan toteutusaika on vuosien 2025 ja 2035 välillä ja se
-              pyrkii ulottumaan jokaiseen suomalaisen yhteiskunnan lokeroon.
-              Tarkastelet tällä hetkellä kampanjan kansalaisille suunnattua
-              sivustoa, joka pureutuu jokapäiväiseen kuluttamiseen. Juuri sinä
-              oletkin avainasemassa Suomen aseman kirjoittamisessa
-              historiankirjoihin. Jäätkö odottamaan, vai aiotko uskaltaa?
-            </p>
-          </div>
-          <div class="timeline-el">
-            <p>
-              Kampanja ei saavuta tavoitteitaan, ja Suomi päättää
-              kollektiivisesti ja tietoisesti jäädä peesaamaan
-              tulevaisuuskehityksen suhteen. Joudumme tanssimaan kehitysvastuun
-              ottaneiden maiden pillien mukaan, kun yhteiskuntaamme järkyttää
-              ulkopuolinen paine - vaikka meille puhdasta vettä ja laidunmaata
-              riittääkin, poliittisesti voimakkaammat maat ja paremman
-              neuvotteluaseman saavuttaneet tahot joutuvat tekemään meistä
-              riippumattomia päätöksiä siitä, minne miljardit ihmiset siirretään
-              ja heidän tarpeidensa tyydyttämiseen vaadittavat resurssit
-              luodaan.
-            </p>
-            <img
-              src={require("./images/webp/smog.webp")}
-              alt="savusumua kaupungissa"
-            />
-          </div>
-          <div class="timeline-el">
-            <p>
-              Suomi on hiilineutraali jo reippaasti ennen muuta Eurooppaa,
-              saatika muuta maailmaa. Suomalainen luontoystävällinen elämäntapa
-              on suosiossa maailmalla, ja maatamme kohtaan suhtaudutaan
-              kunnioituksella ja ihainnoinnilla. Suomalaiset yritykset kulkevat
-              kehityksen aallonharjalla ja luovat hyvinvointia valtakuntaan
-              työpaikkojen, patenttiteknologian ja kestävän talouskasvun
-              muodossa.
-            </p>
-            <img
-              src={require("./images/webp/green-city.webp")}
-              alt="vihreää nurmikkoa"
-            />
-          </div>
+        </header>
+        <div id="menuColor"></div>
+        <div className="transition-wave">
+          <img src="/content/stacked-waves.svg" alt="section transition" />
+        </div>
+        <section className="content">
+          <section className="grid-container">
+            <div className="timeline-el">
+              <div className="block">
+                <p className="plainText" >
+                  Vuoden 2022 marraskuussa järjestetyn Egyptin ilmastokokouksen
+                  jälkimainingeissa nousee esiin ajatus Suomesta ilmastotoimien
+                  johtomaana.
+                  <br />
+                  <br />
+                  Virkamiestyöstö alkaa kansallisen kampanjan aikaansaamiseksi.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <img src={require("./images/uncompressed originals/un-flags.png")} alt="united nations" />
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <img
+                  src={require("./images/uncompressed originals/finnish-parliament.png")}
+                  alt="finnish parliament"
+                />
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <p className="plainText" >
+                  Toukokuussa 2023 hallitus linjaa hallitusohjelmassaan tavoitteen
+                  siitä, että ilmastoteoilla, innovaatioilla ja osaamisella Suomen
+                  ilmastopolitiikasta rakennetaan vientituote.
+                  <br />
+                  <br />
+                  Ensimmäiset visiot kansallisen kampanjan muodosta alkavat
+                  hahmottua. Tässä kohtaa kampanja kulkee vain työnimellä
+                  “Tulevaisuushanke”.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <p className="plainText" >
+                  Tammikuussa 2024 alkaa Hiilineutraali Suomi 2035 -kampanjan
+                  materiaalien levitys mediassa.
+                  <br />
+                  <br />
+                  Kyseessä on kunnianhimoinen kampanja, jonka tavoitteet pyrkivät
+                  aikaansaamaan 1920-luvun yhteiskunnan uusien tuulien kaltaisia
+                  asenteenmuutoksia.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <img src={require("./images/uncompressed originals/social-media.png")} alt="social media" />
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <img src={require("./images/uncompressed originals/crosswalk.png")} alt="crosswalk people" />
+              </div>
+            </div>
+            <div className="timeline-el">
+              <div className="block">
+                <p className="plainText" >
+                  Kampanjan toteutusaika on vuosien 2025 ja 2035 välillä ja se
+                  pyrkii ulottumaan jokaiseen suomalaisen yhteiskunnan lokeroon.
+                  <br />
+                  <br />
+                  Tarkastelet tällä hetkellä kampanjan kansalaisille suunnattua
+                  sivustoa, joka pureutuu jokapäiväiseen kuluttamiseen. Juuri sinä
+                  oletkin avainasemassa Suomen aseman kirjoittamisessa
+                  historiankirjoihin.
+                  <br />
+                  <br />
+                  Jäätkö odottamaan, vai aiotko uskaltaa?
+                </p>
+              </div>
+            </div>
+            <div className="timeline-el arrow-left">
+              <img src={require("./images/arrow.svg")} alt="arrow left" />
+            </div>
+            <div className="timeline-el arrow-right">
+              <img src={require("./images/arrow.svg")} alt="arrow right" />
+            </div>
+            <div className="timeline-el" id="bad">
+              <div className="block">
+                <img src={require("./images/uncompressed originals/smog.png")} alt="smog skyline" />
+                <p className="plainText" >
+                  Kampanja ei saavuta tavoitteitaan, ja Suomi päättää
+                  kollektiivisesti ja tietoisesti jäädä peesaamaan
+                  tulevaisuuskehityksen suhteen.
+                  <br />
+                  <br />
+                  Joudumme tanssimaan kehitysvastuun ottaneiden maiden pillien
+                  mukaan, kun yhteiskuntaamme järkyttää ulkopuolinen paine. Vaikka
+                  meille puhdasta vettä ja laidunmaata riittääkin, poliittisesti
+                  voimakkaammat maat ja paremman neuvotteluaseman saavuttaneet tahot
+                  jyräävät.
+                  <br />
+                  <br />
+                  Ne tekevät meistä riippumattomia päätöksiä siitä, minne miljardit
+                  ihmiset siirretään ja heidän tarpeidensa tyydyttämiseen
+                  vaadittavat resurssit luodaan.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-el" id="good">
+              <div className="block">
+                <img src={require("./images/uncompressed originals/green-city.png")} alt="green city" />
+                <p className="plainText" >
+                  Suomi on hiilineutraali jo reippaasti ennen muuta Eurooppaa,
+                  saatika muuta maailmaa.
+                  <br />
+                  <br />
+                  Suomalainen luontoystävällinen elämäntapa on suosiossa maailmalla,
+                  ja maatamme kohtaan suhtaudutaan kunnioituksella ja
+                  ihainnoinnilla.
+                  <br /><br />
+                  Suomalaiset yritykset kulkevat kehityksen aallonharjalla ja luovat
+                  hyvinvointia valtakuntaan työpaikkojen, patenttiteknologian ja
+                  kestävän talouskasvun muodossa.
+                </p>
+              </div>
+            </div>
+          </section>
         </section>
+        <h1 className="bigHeader" text-align="center" margin-top="20vh">Miten valitset?</h1>
+        <div className="transition-wave" id="end">
+          <img src={require("./images/stacked-waves.svg")} alt="section transition" />
+        </div>
       </body>
     </>
   );
